1c798ef22f6f5d52f43712cc918f36862c16df17
Guilherme - Milhouse <guilherme.milhouse@gmail.com>
2016-09-12 23:42:31 -0300

    "Hibernate Login System" attempt 01 - First commit

---
+++ Ifacefanpage/src/testes/guilherme_login_system/Menu.java
@@ -0,0 +1,9 @@
+package testes.guilherme_login_system;
+
+public class Menu {
+	
+	public static void main(String[] args) {
+		System.out.print("START\n\n    [[ Actions ]]\n\n        1) LOGIN\n        2) SIGN UP ");
+	}
+
+}
