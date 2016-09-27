c5c561510502a7e8b20b7f2f0b93eae6f4c475d6
Administrator <Administrator@192.168.200.218>
2016-09-27 17:52:56 -0300

    Teste inicial com o Spring.

--- Ifacefanpage/src/testes/HelloWorld.java
+++ Ifacefanpage/src/testes/HelloWorld.java
@@ -1,8 +1,19 @@
 package testes;
 
 public class HelloWorld {
+	private String message;
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
+
+	/*
 	public static void main(String[] args) {
 		System.out.println("Hello world!"+" Testing another time");
 	}
-
+	 */
 }
