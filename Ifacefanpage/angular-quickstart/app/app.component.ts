005e5ab21eb3e20d1cf8d2d60868a884748addb9
Administrator <Administrator@192.168.200.218>
2016-10-20 15:40:46 -0300

    Configuração rápida do Angular

---
+++ Ifacefanpage/angular-quickstart/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+    template: '<h1><p>My First Angular App</p>My SECOND Angular App</h1>'
+})
+export class AppComponent { }
+
